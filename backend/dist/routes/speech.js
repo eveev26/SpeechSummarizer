10993ea6c213814bde7fd23cf1e23deef09b9179
CherylZ4 <112735556+CherylZ4@users.noreply.github.com>
2023-08-25 21:32:20 -0400

    Edited speech-to-text.service to use protos and changed the encoding to FLAC. Also completed the textRouter post call in speech.ts.

--- backend/dist/routes/speech.js
+++ backend/dist/routes/speech.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -14,9 +37,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.textRouter = void 0;
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const textService = __importStar(require("../services/speech-to-text.service.js"));
 exports.textRouter = express_1.default.Router();
 exports.textRouter.use(express_1.default.json());
 exports.textRouter.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const gcsUri = req.body.file_url;
+        // Print the gcsUri value to the console
+        console.log("Received gcsUri:", gcsUri);
+        const response = yield textService.transcribe(gcsUri);
+        res.status(201).json(response);
+    }
+    catch (e) {
+        if (typeof e === "object" && e instanceof Error && e.message) {
+            res.status(500).send(e.message);
+        }
+        else {
+            res.status(500).send("An unknown error occurred.");
+        }
+    }
 }));
